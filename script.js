a60921e153159e080f783a04075fdaf51aea043b
Tiago Pires <tiagopires2501@outlook.com>
2024-10-04 21:50:41 -0400

    Imagem 2
    Agora a imagem 2 carrega no html

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let descobrirNome = function(){
     let nome = document.getElementById("nome").value;
     let saida = document.getElementById("resultado");
     let imagem1 = document.getElementById("imagem1");
+    let imagem2 = document.getElementById("imagem2");
 
     imagem1.src = "src/img/img1.jpeg"
+    imagem2.src = "src/img/img2.jpeg"
     return saida.innerHTML = nome
 };
