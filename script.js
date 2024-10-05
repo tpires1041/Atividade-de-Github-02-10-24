3119d7d7e35f2f357b4df38de57f3bbb1fda96a5
Tiago Pires <tiagopires2501@outlook.com>
2024-10-04 21:47:47 -0400

    Agora a imagem carrega
    Agora a imagem 1 está carregando

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let descobrirNome = function(){
     let nome = document.getElementById("nome").value;
     let saida = document.getElementById("resultado");
-    let imagem1 = document.getElementById("imagem");
+    let imagem1 = document.getElementById("imagem1");
 
-    imagem1.src = "\src\img\img1.jpeg"
+    imagem1.src = "src/img/img1.jpeg"
     return saida.innerHTML = nome
 };
