a6ce361cbe0cedc8f74011692fce9c79868434d8
Tiago Pires <tiagopires2501@outlook.com>
2024-10-04 21:42:23 -0400

    Imagens adicionadas no html
    Agora existem tags para as imagens

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let descobrirNome = function(){
     let nome = document.getElementById("nome").value;
     let saida = document.getElementById("resultado");
+    let imagem1 = document.getElementById("imagem");
 
+    imagem1.src = "\src\img\img1.jpeg"
     return saida.innerHTML = nome
 };
